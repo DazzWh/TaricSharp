dc9e2c0c50428e24cbd692387c4c37485ab676b5
Darrell Whittall <darrellwhittall@gmail.com>
2020-03-10 13:19:53 +0000

    Removed useless comments

--- Program.cs
+++ Program.cs
@@ -22,14 +22,14 @@ namespace TaricSharp
         private static IServiceProvider BuildServiceProvider() => new ServiceCollection()
             .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
             {
-                LogLevel = LogSeverity.Verbose, // Tell the logger to give Verbose amount of info
-                MessageCacheSize = 1000 // Cache 1,000 messages per channel
+                LogLevel = LogSeverity.Verbose,
+                MessageCacheSize = 1000 
             }))
 
             .AddSingleton(new CommandService(new CommandServiceConfig
             {
-                LogLevel = LogSeverity.Verbose, // Tell the logger to give Verbose amount of info
-                DefaultRunMode = RunMode.Async, // Force all commands to run async by default
+                LogLevel = LogSeverity.Verbose,
+                DefaultRunMode = RunMode.Async
             }))
             .AddSingleton<LoggingService>()
             .AddSingleton<CommandHandler>()
