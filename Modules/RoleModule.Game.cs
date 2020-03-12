41bc5c80c5e4c2d05005d597d76ae2c97884206a
Darrell Whittall <darrellwhittall@gmail.com>
2020-03-12 09:06:42 +0000

    Remove empty game roles on role removal

--- Modules/RoleModule.Game.cs
+++ Modules/RoleModule.Game.cs
@@ -11,7 +11,7 @@ namespace TaricSharp.Modules
         [Command("game")]
         [Summary("Adds or removes a GameRole to user")]
         public async Task GameAsync(
-            [Summary("Game name")] string gameName)
+            [Summary("Game name")] [Remainder] string gameName)
         {
             var gameRole = Context.Guild.Roles
                 .FirstOrDefault(role => role.Color == _gameRoleColor &&
@@ -29,13 +29,28 @@ namespace TaricSharp.Modules
                 if (gameRole.Members.Contains(user))
                 {
                     await user.RemoveRoleAsync(gameRole);
+                    await DeleteEmptyGameRoles();
                     await ReplyAsync($"{gameName} removed from {Context.User.Username}");
+
                 }
                 else
                 {
                     await user.AddRoleAsync(gameRole);
                     await ReplyAsync($"{gameName} added to {Context.User.Username}");
                 }
+            }
+        }
+
+        private async Task DeleteEmptyGameRoles()
+        {
+            var empty =
+                Context.Guild.Roles.Where(
+                    role => role.Color == _gameRoleColor &&
+                            !role.Members.Any());
+
+            foreach (var role in empty)
+            {
+                await Context.Guild.GetRole(role.Id).DeleteAsync();
             }
         }
     }
