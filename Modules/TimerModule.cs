5e1e7113ac5af1f498dd50dd513ef6ca573b5c67
Darrell Whittall <darrellwhittall@gmail.com>
2020-06-16 16:06:44 +0100

    Rough outline

---
+++ Modules/TimerModule.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+using TaricSharp.Services;
+
+namespace TaricSharp.Modules
+{
+    public class TimerModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly TimerService _timerService;
+
+        public TimerModule(
+            TimerService timerService)
+        {
+            _timerService = timerService;
+        }
+
+        [Command("timer")]
+        [Summary("Initiates a timer")]
+        [Remarks("")]
+        public async Task InitiateTimer(
+            int mins)
+        {
+            // Check input is valid
+            // Give user feedback
+            // Start timer service
+
+            _timerService.CreateTimerMessage(Context, mins);
+        }
+    }
+}
